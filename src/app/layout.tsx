d450c5c714c0c5e39eb9ddaa08b0bc34607c56f5
nofloop <nofloop@gmail.com>
2024-06-25 21:01:22 +0300

    Add select link functinality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
       <body>
         <Providers>
           <TopNav></TopNav>
-          {children}
+          <main className='container mx-auto p-10'>
+            {children}
+          </main>
         </Providers> 
       </body>
     </html>
