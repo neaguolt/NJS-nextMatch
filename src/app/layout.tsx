558bd5b489d1a3ff201d886665ad89ec51bb6eca
nofloop <nofloop@gmail.com>
2024-06-25 15:30:07 +0300

    Add a navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "@/components/Providers";
+import TopNav from "@/components/navbar/TopNav";
 
 
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Providers>
+          <TopNav></TopNav>
           {children}
         </Providers> 
       </body>
