838086587fecea45cc996dc78d70f66aac4e1d70
nofloop <nofloop@gmail.com>
2024-06-25 17:06:48 +0300

    Use () to exclude folder from route

--- src/components/navbar/TopNav.tsx
+++ src/components/navbar/TopNav.tsx
@@ -28,7 +28,7 @@ export default function TopNav() {
             <NavbarItem as={Link} href='/messages'>Messages</NavbarItem>
         </NavbarContent>
         <NavbarContent justify='end'>
-            <Button as={Link} href='/auth/login' variant='bordered' className='text-white'>Login</Button>
+            <Button as={Link} href='/login' variant='bordered' className='text-white'>Login</Button>
             <Button as={Link} href='/register'variant='bordered' className='text-white'>Register</Button>
         </NavbarContent>
     </Navbar>
