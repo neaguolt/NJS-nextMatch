d4535834a2c67a5961450ecda585cb6581d8c5fb
neaguolt <meaguolt@gmail.com>
2024-06-26 10:06:33 +0300

    Address reviewer feedback: fix padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <body>
         <Providers>
           <TopNav></TopNav>
-          <main className='container mx-auto p-10'>
+          <main className='container mx-auto p-20'>
             {children}
           </main>
         </Providers> 
