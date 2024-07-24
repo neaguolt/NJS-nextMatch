0a798150ee7633ce91845941a200222f1c1caf5e
neaguolt <meaguolt@gmail.com>
2024-07-24 12:09:00 +0300

    32 Add signOut functionality
    This happens on home page inside a form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,38 @@
+import { auth, signOut } from "@/auth";
 import { Button } from "@nextui-org/react";
-import Link from "next/link";
 import { FaRegSmile } from "react-icons/fa";
 
-export default function Home() {
+export default async function Home() {
+  
+  const session = await auth();
+
   return (
     <div>
       <h1 className='text-3xl font-semibold text-center'>Welcome to my app! </h1>
-      <Button 
-        as={Link}
-        href='/members'
-        color='primary' 
-        variant='bordered' 
-        startContent={<FaRegSmile />}>
-        Click ME!
-      </Button>
+      <h3 className='text-2xl font-semibold'>User session data:</h3>
+      {
+        session ? 
+        (
+          <div>
+            <pre>{JSON.stringify(session, null, 2)}</pre>
+            <form action={ async () => {
+              'use server';
+              await signOut();
+            }}>
+              <Button 
+                type='submit'
+                color='primary' 
+                variant='bordered' 
+                startContent={<FaRegSmile />}>
+                Sign Out
+              </Button>  
+            </form>
+          </div>
+        ) :
+        (
+          <div>Not signed in</div>
+        )
+      }
     </div>
   );
 }
